ad2e0faf6b26ecf5102414febb647d8057052e4d
OFFICEKIEV\d.sabelnykov <d.sabelnykov@temabit.com>
2024-12-02 15:28:30 +0200

    Change military tax

--- SQL/1102.fn_ClcMilitary.sql
+++ SQL/1102.fn_ClcMilitary.sql
@@ -4,12 +4,17 @@ if exists (select * from dbo.sysobjects where id = OBJECT_ID(N'[dbo].[fn_ClcMili
 GO
 Create function [dbo].[fn_ClcMilitary] 
 (
-	@inGrossInc          numeric(10,2)     --Валовый доход  
+	@inGrossInc          numeric(10,2),    --Валовый доход 
+    @inCalcDate          DATE              --Дата расчета налога  
 )
 RETURNS numeric(10,2)
 AS   
 BEGIN
-  Declare @MilitaryPrc numeric(10,4) = 0.015;
+  
+  DECLARE @MilitaryPrc numeric(10,4);
+  SET @MilitaryPrc = CASE WHEN @inCalcDate >= '2024-12-01' THEN 0.05
+                          ELSE 0.015
+                      END
 
   set @inGrossInc = coalesce(@inGrossInc, 0);
 
